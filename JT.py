157ba9ffcaf54deedd175c7ad4a7a1b5e3753985
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-09-24 20:07:53 +0100

    Update JT.py

--- JT.py
+++ JT.py
@@ -1,7 +1,7 @@
-First = input("do you want to kill")
+'First = input("do you want to kill")'
 if First==yes or First==Yes:
-Linktext = print("send this link to victim")
-link = print("https://bit.ly/3fX8ljZ")
+'Linktext = print("send this link to victim")'
+'link = print("https://bit.ly/3fX8ljZ")'
 elif First==no or First==No:
 clear
 print(Name)
