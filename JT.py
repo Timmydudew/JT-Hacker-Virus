2bfd6717354903cb963fa4c6d8b839d8e4d8d703
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-09-25 10:52:08 +0100

    Update JT.py

--- JT.py
+++ JT.py
@@ -1,8 +1,10 @@
-def virus:
-'First = input("do you want to kill")'
-if First==yes or First==Yes:
-'Linktext = print("send this link to victim")'
-'link = print("https://bit.ly/3fX8ljZ")'
-elif First==no or First==No:
-clear
+def banner():
+             print(
+def Virus() :
+             First = input("do you want to kill")'
+             If First==yes or First==Yes:
+             Linktext = print("send this link to victim")'
+             link = print("https://bit.ly/3fX8ljZ")'
+             elif First==no or First==No:
+             clear
 print(Name)
